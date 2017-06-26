4ef5f5864b5de3ca05ab91a7939baa84c7f18649
LAPTOP-7TT2G4VV\cassio <cassio.csr@hotmail.com>
2017-06-26 14:43:01 -0300

    Add aula 10

---
+++ aula10/Variaveis.java
@@ -0,0 +1,32 @@
+package aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		//convenção java
+		int idade = 20;
+		String nome = "Cassio";
+		String nomeDoMeuCachorro = "Duque";
+		String ano2014;
+		
+		//aceito mas não utilizado
+		 int _idade;
+		 int $idade;
+		 
+		 //não é convençao java
+		 String nome_do_meu_cachorro;
+		 String NomeDoMeuCachorro;
+		 String nomeDoMeucachorro;
+		 
+		 idade = 25;
+		 System.out.println("Idade = " + idade);
+		 System.out.println("Nome = " + nome);
+		 
+		 // má prática
+		 int a = 10;
+		 String b = "Cassio";
+		 // É importante que as vaáveis tenham nomes significativos
+	}
+
+}
