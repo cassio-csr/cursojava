adaa352b942afc82e93cf426bcc90ec8a5761ac4
LAPTOP-7TT2G4VV\cassio <cassio.csr@hotmail.com>
2017-06-26 16:40:26 -0300

    Add aula 11

---
+++ aula11/VariaveisChar.java
@@ -0,0 +1,15 @@
+package aula11;
+
+public class VariaveisChar {
+
+	public static void main(String[] args) {
+		
+		char o = 'o';		
+		char i = 'i';
+		char intrrogacao = 0x003F;
+		
+		System.out.println(""+o+i+intrrogacao);
+
+	}
+
+}
